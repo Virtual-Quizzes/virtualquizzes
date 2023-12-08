08065f755f4c3b614f2f2b7743c0186b2df3cf03
Jose Zegarra <jzegarram2000@gmail.com>
2023-12-07 21:44:03 -0500

    cors config

--- src/main/frontend/src/web/vq-axios-client.ts
+++ src/main/frontend/src/web/vq-axios-client.ts
@@ -1,6 +1,6 @@
 import { AxiosClientFactory } from './factory/axios-client.factory';
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://virtualquizzes-production-b199.up.railway.app";
 // export const API_BASE_URL = "/";
 export const API_VERSION = 'api/v1';
 //${API_BASE_URL}
--- src/main/java/com/juezcachimbo/virtualquizzes/config/SecurityConfig.java
+++ src/main/java/com/juezcachimbo/virtualquizzes/config/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -20,9 +25,24 @@ public class SecurityConfig {
     private final AuthenticationProvider authProvider;
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         return http
+                .cors().and()
                 .csrf(csrf ->
                         csrf
                                 .disable())
